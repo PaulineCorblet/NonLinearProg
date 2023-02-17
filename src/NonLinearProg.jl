42d0e7a0827e0e552f49ca6fe88da074762c1361
Pauline CORBLET <pauline.corblet@sciencespo.fr>
2023-02-17 12:10:27 +0100

    pb def outisde of if

--- src/NonLinearProg.jl
+++ src/NonLinearProg.jl
@@ -148,18 +148,18 @@ function fmincon(f, x0; A = nothing, b = nothing, Aeq = nothing, beq = nothing,
         end
         J_struct = J_struct[:]
         println(string("Number of nonlinear constraints:         ",K,"."))
-        prob       = OptProblem(f,g,h,J,J_struct) 
-        block_data = MOI.NLPBlockData(MOI.NLPBoundsPair.(cons_lb, cons_ub), prob, true)
-        MOI.set(optimizer, MOI.NLPBlock(), block_data)
     else
         J_struct = nothing
         cons_lb  = Float64[]
         cons_ub  = Float64[]
-        prob       = OptProblem(f,g,h,J,J_struct) 
-        block_data = MOI.NLPBlockData(MOI.NLPBoundsPair.(cons_lb, cons_ub), prob, true)
-        MOI.set(optimizer, MOI.NLPBlock(), block_data)
+
         println(string("Number of nonlinear constraints:         ",0,"."))
     end
+
+    # Pass non-linear constraints & objective function
+    prob       = OptProblem(f,g,h,J,J_struct) 
+    block_data = MOI.NLPBlockData(MOI.NLPBoundsPair.(cons_lb, cons_ub), prob, true)
+    MOI.set(optimizer, MOI.NLPBlock(), block_data)
   
     # Optimize
     MOI.set(optimizer, MOI.ObjectiveSense(), MOI.MIN_SENSE)
