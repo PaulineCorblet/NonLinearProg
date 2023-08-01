5069f0f2daaec71a6a0c36453d7bc1a66c964aeb
Simon Weber <simon.weber@york.ac.uk>
2023-08-01 19:21:32 +0200

    Update derivative.jl

--- src/derivative.jl
+++ src/derivative.jl
@@ -81,7 +81,7 @@ end
 function derivative_par(f::Function, x; epsilon = nothing, method = "forward", print_level = 0)
 
     # Insert function
-    @everywhere function insert(x,xk,k)
+    Distributed.@everywhere function insert(x,xk,k)
         new_x = copy(x)
         new_x[k] = xk
         return new_x
